0b1c1d3739ab59e8962136363a524f50be250d34
uy <97729133+prestar5@users.noreply.github.com>
2023-09-14 15:46:38 +0100

    hehe

--- dl.py
+++ dl.py
@@ -5,7 +5,28 @@ print('***This script requires yt-dlp and ffmpeg!***')
 print('For a full list of supported sites, go to this link: https://github.com/yt-dlp/yt-dlp/blob/release/supportedsites.md \n')
 
 # asks user to input url
-input_url = input("Enter a URL: ")
+input_url = input("Enter a URL: ") #>todo add validation later
+
+#here we give the user the option to download their file as an mp3 file or a video file also i just cut and pasted this from the internet haha
+ options = ['video', 'audio']
+
+ user_input = ''
+
+ input_message = "Options:\n"
+
+ for index, item in enumerate(options):
+     input_message += f'{index+1}) {item}\n'
+
+ input_message += 'Type in number corresponding to option: '
+
+ while user_input not in map(str, range(1, len(options) + 1)):
+     user_input = input(input_message)
+
+#presets
+ video = {
+     'format': 'mp4/bestvideo'
+     #>todo: finish settings
+     }
 
 # presets
 audio = {
